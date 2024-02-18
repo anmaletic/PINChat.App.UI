e8db2407f93e2e8a30a1024c1e0d4601d18e88e1
AnMal <junkie_@msn.com>
2024-02-18 22:50:21 +0100

    Initial commit

---
+++ PINChat.App.Avalonia/PINChat.App.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PINChat.App.Avalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ PINChat.App.Avalonia/PINChat.App.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PINChat.App.Avalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
