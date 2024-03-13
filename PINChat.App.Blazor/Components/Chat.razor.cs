4cd1d08a2607b4618bc05c150f96940aa9af5c67
AnMal <junkie_@msn.com>
2024-03-13 07:20:40 +0100

    Implement Chat component

---
+++ PINChat.App.Blazor/Components/Chat.razor.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Components;
+using PINChat.App.Library.Api;
+using PINChat.App.Library.Models;
+using PINChat.App.Library.Services;
+
+namespace PINChat.App.Blazor.Components;
+
+public partial class Chat
+{
+    [Inject] private IUserEndpoint UserEndpoint { get; set; } = null!;
+    [Inject] private IImageEndpoint ImageEndpoint { get; set; } = null!;
+    [Inject] private ILoggedInUserModel LoggedInUser { get; set; } = null!;
+    [Inject] private IRecipientModel Recipient { get; set; } = null!;
+    [Inject] private IChatService ChatService { get; set; } = null!;
+
+    private UserModel UserTarget { get; set; } = new();
+    private string SelectedHeaderItem { get; set; } = "Messages";
+    private bool IsModalVisible { get; set; }
+    private bool IsReadOnly { get; set; }
+    
+    
+    protected override async Task OnInitializedAsync()
+    {
+        Recipient.PropertyChanged += async (sender, args) => await InvokeAsync(StateHasChanged);
+        await SetupChatHub();
+    }
+    private async Task SetupChatHub()
+    {
+        if (!ChatService.IsConnected && !string.IsNullOrEmpty(LoggedInUser.Id))
+        {
+            await ChatService.Connect();
+            await ChatService.Login(LoggedInUser.Id);
+            
+            List<string> groups = LoggedInUser.Groups.Select(x => x.Id).ToList()!;
+            foreach (var group in groups)
+            {
+                await ChatService.AddGroup(group);
+            }
+        }
+    }
+    
+    private void SelectHeaderItem(string selection)
+    {
+        Recipient.Reset();
+        
+        if (SelectedHeaderItem == selection)
+        {
+            SelectedHeaderItem = "Messages";
+            return;
+        }
+        SelectedHeaderItem = selection;
+    }
+
+    private async Task ShowDetails(string target)
+    {
+        switch (target)
+        {
+            case "User":
+                UserTarget = (LoggedInUser as UserModel)!;
+                IsReadOnly = false;
+                IsModalVisible = true;
+                break;
+            case "Recipient":
+                if (Recipient.Type == "User")
+                {
+                    // replace with UserEndpoing.GetById(Recipient.Id)
+                    UserTarget = (await UserEndpoint.GetAll()).FirstOrDefault(x=>x.Id == Recipient.Id)!;
+                    IsReadOnly = true;
+                    IsModalVisible = true;
+                }
+                break;
+        }
+    }
+    private void OnProfileClosed()
+    {
+        IsModalVisible = false;
+    }
+    private void OnUpdate()
+    {
+        LoggedInUser.AvatarPath = ImageEndpoint.GetUserImage((LoggedInUser as UserModel)!);
+    }
+
+}
