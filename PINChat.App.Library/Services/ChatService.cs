3fe562013535af47534000109dbf46ae490563be
AnMal <junkie_@msn.com>
2024-03-13 06:46:53 +0100

    Implement service
    Implement service for connecting to
    Chat server

--- PINChat.App.Library/Services/ChatService.cs
+++ PINChat.App.Library/Services/ChatService.cs
@@ -1,18 +1,71 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR.Client;
+using PINChat.App.Library.Models;
 
 namespace PINChat.App.Library.Services;
 
 public class ChatService : IChatService
 {
-    public HubConnection? HubConnection { get; set; } = null!;
+    public bool IsConnected => _hubConnection.State == HubConnectionState.Connected;
+    public event Action<string, MessageDtoModel>? MessageReceived;
+    
+    private readonly HubConnection _hubConnection;
 
-    public async Task AddGroup(string groupId)
+
+    public ChatService()
     {
-        await HubConnection!.InvokeAsync("AddGroup", groupId);
+        _hubConnection = new HubConnectionBuilder()
+            .WithUrl("https://pinchat-server.anmal.dev/chathub", options =>
+            {
+                options.AccessTokenProvider = async () => await Task.FromResult("YourAccessToken");
+                options.Transports = HttpTransportType.WebSockets;
+            })
+            .WithAutomaticReconnect([TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(5)])
+            .Build();
+        
+        _hubConnection.On<string, MessageDtoModel>("ReceiveMessage", (action, message) => MessageReceived?.Invoke(action, message));
     }
     
+    public async Task<bool> Connect()
+    {
+        try
+        {
+            await _hubConnection.StartAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public async Task<bool> Login(string userName)
+    {
+        try
+        {
+            await _hubConnection.InvokeAsync("Login", userName);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public async Task AddGroup(string groupId)
+    {
+        await _hubConnection.InvokeAsync("AddGroup", groupId);
+    }
     public async Task RemoveGroup(string groupId)
     {
-        await HubConnection!.InvokeAsync("RemoveGroup", groupId);
+        await _hubConnection.InvokeAsync("RemoveGroup", groupId);
+    }
+    
+    public async Task SendSingleMessage(MessageDtoModel dtoMessage)
+    {
+        await _hubConnection.SendAsync("MessageSingle", "AddSingle", dtoMessage);
+    }
+    public async Task SendGroupMessage(MessageDtoModel dtoMessage)
+    {
+        await _hubConnection.SendAsync("MessageGroup", "AddGroup", dtoMessage);
     }
 }
--- PINChat.App.Library/Services/IChatService.cs
+++ PINChat.App.Library/Services/IChatService.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using PINChat.App.Library.Models;
 
 namespace PINChat.App.Library.Services;
 
 public interface IChatService
 {
-    HubConnection? HubConnection { get; set; }
     Task AddGroup(string groupId);
     Task RemoveGroup(string groupId);
+    Task SendSingleMessage(MessageDtoModel dtoMessage);
+    Task SendGroupMessage(MessageDtoModel dtoMessage);
+    Task<bool> Connect();
+    Task<bool> Login(string userName);
+    bool IsConnected { get; }
+    event Action<string, MessageDtoModel>? MessageReceived;
 }
