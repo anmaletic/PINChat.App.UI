e17ddfcdf48c9113538c0989ae58ef0cc7bb5e2b
AnMal <junkie_@msn.com>
2024-03-13 06:53:13 +0100

    Add Css attribute to MessageModel

--- PINChat.App.Library/Models/MessageModel.cs
+++ PINChat.App.Library/Models/MessageModel.cs
@@ -7,8 +7,12 @@ public class MessageModel
     public string? TargetId { get; set; }
     public string? SourceId { get; set; }
     public string? SourceName { get; set; }
-    public string? SourceAvatar { get; set; }
+    public string? AvatarPath { get; set; }
     public string? Content { get; set; }
     public string? Image { get; set; }
     public bool IsOrigin { get; set; }
+    
+    // todo: create MessageModel in blazor project with Css attribute
+    // todo: remove Css attribute
+    public string Css => IsOrigin ? "right-aligned" : "left-aligned";
 }
