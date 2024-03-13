495774d735529b63e21ca05c4a44dd2c604771e0
AnMal <junkie_@msn.com>
2024-03-13 07:01:44 +0100

    Register Dependencies

--- PINChat.App.Blazor/Program.cs
+++ PINChat.App.Blazor/Program.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PINChat.App.Blazor;
+using PINChat.App.Library.Api;
+using PINChat.App.Library.Models;
+using PINChat.App.Library.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<IApiHelper, ApiHelper>();
+builder.Services.AddSingleton<ILoggedInUserModel, UserModel>();
+builder.Services.AddSingleton<IRecipientModel, TargetModel>();
+
+builder.Services.AddScoped<IUserEndpoint, UserEndpoint>();
+builder.Services.AddScoped<IGroupEndpoint, GroupEndpoint>();
+
+builder.Services.AddSingleton<IChatService, ChatService>();
+
+builder.Services.AddScoped<IRegistrationEndpoint, RegistrationEndpoint>();
+
+builder.Services.AddScoped<IImageEndpoint, ImageEndpoint>();
+builder.Services.AddScoped<IMessageEndpoint, MessageEndpoint>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
