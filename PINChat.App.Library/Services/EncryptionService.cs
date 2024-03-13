5825d21b6b4fbc4bd7cb5ed644615a1d3580434b
AnMal <junkie_@msn.com>
2024-03-13 06:43:50 +0100

    Remove EncryptionService
    Move encryption to api

--- PINChat.App.Library/Services/EncryptionService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-
-namespace PINChat.App.Library.Services;
-
-public class EncryptionService
-{
-    // create service to encrypt and decrypt data
-}
