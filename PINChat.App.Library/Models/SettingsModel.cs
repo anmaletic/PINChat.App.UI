733fcfad90901e593a08a038d4f0128c8ae4502f
AnMal <junkie_@msn.com>
2024-03-13 06:53:22 +0100

    Implement SettingsModel

---
+++ PINChat.App.Library/Models/SettingsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PINChat.App.Library.Models;
+
+public class SettingsModel
+{
+    public int Id { get; set; }
+    public string? Key { get; set; }
+    public string? Value { get; set; }
+}
