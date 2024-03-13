13f6c1e47c7e0b76b20629888f57fab07b73f295
AnMal <junkie_@msn.com>
2024-03-13 07:00:20 +0100

    Implement AuthenticationModel

---
+++ PINChat.App.Blazor/Models/AuthenticationUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PINChat.App.Blazor.Models;
+
+public class AuthenticationUserModel
+{
+    [Required(ErrorMessage = "Nije upisano korisničko ime!")]
+    public string? UserName { get; set; }
+    
+    [Required(ErrorMessage = "Nije upisana lozinka!")]
+    public string? Password { get; set; }
+    
+    public void Reset()
+    {
+        UserName = null;
+        Password = null;
+    }
+}
