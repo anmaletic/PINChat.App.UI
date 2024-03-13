1ef2f32b3c2555d8834f4576f30e5e4b0329867f
AnMal <junkie_@msn.com>
2024-03-13 06:58:55 +0100

    Implement ImageEndpoint

---
+++ PINChat.App.Library/Api/IImageEndpoint.cs
@@ -0,0 +1,9 @@
+﻿using PINChat.App.Library.Models;
+
+namespace PINChat.App.Library.Api;
+
+public interface IImageEndpoint
+{
+    string GetUserImage(UserModel user);
+    string GetGroupImage(string id);
+}
---
+++ PINChat.App.Library/Api/ImageEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using PINChat.App.Library.Models;
+
+namespace PINChat.App.Library.Api;
+
+public class ImageEndpoint : IImageEndpoint
+{
+    public string GetUserImage(UserModel user)
+    {
+        var api = "https://pinchat-api.anmal.dev";
+        
+        return $"{api}/api/Image/GetUserImage/{user.Id}?t={DateTime.Now.Ticks}";
+    }
+
+    public string GetGroupImage(string id)
+    {
+        var api = "https://pinchat-api.anmal.dev";
+        
+        return $"{api}/api/Image/GetGroupImage/{id}?t={DateTime.Now.Ticks}";
+    }
+}
